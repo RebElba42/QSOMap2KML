3d3ba2bf6a2562163489b51182a44c5d55d090f4
REBMATE13\reb <rebelba42@gmail.com>
2025-05-22 17:56:27 +0200

    Update to Nutika build system

---
+++ build_win_nutika.cmd
@@ -0,0 +1,16 @@
+REM Build Windows Executable mit Nuitka und UPX
+REM set PATH=C:\upx;%PATH%
+nuitka ^
+  --standalone ^
+  --onefile ^
+  --enable-plugin=pyqt6 ^
+  --windows-console-mode=disable ^
+  --include-data-dir=resources=resources ^
+  --include-data-dir=config=config ^
+  --include-data-file=resources\xyzservices\data\providers.json=xyzservices\data\providers.json ^
+  --include-data-dir=resources\translations=resources\translations ^
+  --include-data-dir=resources\icons=resources\icons ^
+  --windows-icon-from-ico="resources\icons\Flow block.ico" ^
+  --output-filename=QSOMap2KML.exe ^
+  --output-dir=dist  ^
+  main.py
