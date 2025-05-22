f701d035f761867863705b3531c24906458c95b4
REBMATE13\reb <rebelba42@gmail.com>
2025-05-22 18:31:55 +0200

    Fixed resource_path for Nuitka

--- utils/app_utils.py
+++ utils/app_utils.py
@@ -2,10 +2,12 @@ import sys
 import os
 
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+    """ Get absolute path to resource, works for dev, PyInstaller and Nuitka """
+    # Nuitka setzt sys.frozen == True, aber kein _MEIPASS!
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
+    if getattr(sys, 'frozen', False):
+        # Nuitka: Ressourcen liegen im aktuellen Arbeitsverzeichnis
+        return os.path.join(os.path.dirname(sys.executable), relative_path)
     return os.path.join(os.path.abspath("."), relative_path)
-
-
   
